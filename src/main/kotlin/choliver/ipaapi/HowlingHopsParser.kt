3433eee2775b9bd96f05c4e1540d9cc29dece1d6
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-06-19 21:05:23 +0100

    Eliminate apparel

--- src/main/kotlin/choliver/ipaapi/HowlingHopsParser.kt
+++ src/main/kotlin/choliver/ipaapi/HowlingHopsParser.kt
@@ -4,18 +4,19 @@ import org.jsoup.nodes.Document
 import java.net.URI
 
 class HowlingHopsParser : Parser {
-  override fun parse(doc: Document) =
-    doc.select(".wc-block-grid__product")
-      .map { el ->
-        val a = el.selectFirst(".wc-block-grid__product-link")
-        Item(
-          url = URI(a.attr("href").trim()),
-          name = a.selectFirst(".wc-block-grid__product-title").text().trim(),
-          price = el.select(".woocommerce-Price-amount")
-            .filterNot { it.parent().tagName() == "del" }
-            .first()
-            .ownText()
-            .toBigDecimal()
-        )
-      }
+  override fun parse(doc: Document) = doc
+    .selectFirst(".wc-block-handpicked-products") // Avoid apparel
+    .select(".wc-block-grid__product")
+    .map { el ->
+      val a = el.selectFirst(".wc-block-grid__product-link")
+      Item(
+        url = URI(a.attr("href").trim()),
+        name = a.selectFirst(".wc-block-grid__product-title").text().trim(),
+        price = el.select(".woocommerce-Price-amount")
+          .filterNot { it.parent().tagName() == "del" }
+          .first()
+          .ownText()
+          .toBigDecimal()
+      )
+    }
 }
