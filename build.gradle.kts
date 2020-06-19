a24ff5afc9999191e28ffd2d0f58aeca3f6e0153
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-06-19 19:01:56 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
+
+plugins {
+  base
+  kotlin("jvm") version "1.3.72"
+}
+
+group = "choliver.ipaapi"
+version = "0.0.0"
+
+repositories {
+  mavenCentral()
+}
+
+tasks.test {
+  useJUnitPlatform()
+  testLogging {
+    events(FAILED)
+  }
+}
+
+dependencies {
+  implementation(kotlin("stdlib"))
+
+  testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
+  testImplementation("org.hamcrest:hamcrest-library:2.2")
+  testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
+  // byte-buddy 1.9.10 (pulled in by Mockito) behaves badly with Java 13 - see https://github.com/mockk/mockk/issues/397
+  testImplementation("net.bytebuddy:byte-buddy:1.10.6")
+}
+
