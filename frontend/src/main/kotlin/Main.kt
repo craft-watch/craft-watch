f92f5506a265189a73876074ffcc5a58a096920e
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-06-20 12:16:42 +0100

    WIP

--- frontend/src/main/kotlin/Main.kt
+++ frontend/src/main/kotlin/Main.kt
@@ -1,19 +1,36 @@
-import kotlinx.html.a
-import kotlinx.html.div
+import kotlinx.html.*
 import kotlinx.html.dom.append
-import kotlinx.html.h1
-import kotlinx.html.p
 import kotlin.browser.document
 
 fun main() {
   document.body!!.append.div {
-    h1 {
-      +"Welcome to NEAPI"
-    }
-    p {
-      +"Link to something gimpy: "
-        a("https://github.com/oliver-charlesworth/nespot") {
-        +"Gimp"
+    h1 { +"Welcome to NEAPI" }
+    table {
+      thead {
+        tr {
+          th { +"Brewery" }
+          th { +"Name" }
+          th { +"Price" }
+        }
+      }
+      tbody {
+        tr {
+          td { +"Villages" }
+          td { a("https://example.invalid") { +"Rodeo" } }
+          td { +"£3.20" }
+        }
+
+        tr {
+          td { +"Villages" }
+          td { a("https://example.invalid") { +"Rafiki" } }
+          td { +"£3.30" }
+        }
+
+        tr {
+          td { +"Howling Hops" }
+          td { a("https://example.invalid") { +"Gimp" } }
+          td { +"£5.80" }
+        }
       }
     }
   }
