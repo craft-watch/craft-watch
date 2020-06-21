d060d99a64964c0255ae38fccdec235df3f1b3da
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-06-21 10:16:24 +0100

    Add --depth 1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ set -eu
 git config user.email "bot@oliver-charlesworth.github.io"
 git config user.name "CircleCI Bot"
 
-git clone git@github.com:oliver-charlesworth/oliver-charlesworth.github.io.git website
+git clone -–depth 1 git@github.com:oliver-charlesworth/oliver-charlesworth.github.io.git website
 cd website
 git rm -r .
 cp ../build/distributions/* .
