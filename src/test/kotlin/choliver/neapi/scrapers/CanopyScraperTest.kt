fc1125823e257d75dbab94283b850e69d4cdf8b0
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-06-23 00:44:16 +0100

    Add basic Canopy scraper (#36)

---
+++ src/test/kotlin/choliver/neapi/scrapers/CanopyScraperTest.kt
@@ -0,0 +1,48 @@
+package choliver.neapi.scrapers
+
+import choliver.neapi.ParsedItem
+import choliver.neapi.executeScraper
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.net.URI
+import kotlin.text.RegexOption.IGNORE_CASE
+
+class CanopyScraperTest {
+  companion object {
+    private val ITEMS = executeScraper(CanopyScraper())
+  }
+
+  @Test
+  fun `finds all the beers`() {
+    assertEquals(7, ITEMS.size)
+  }
+
+  @Test
+  fun `extracts beer details`() {
+    assertEquals(
+      ParsedItem(
+        name = "Brockwell IPA",   // ABV removed from name
+        perItemPrice = 2.50,
+        abv = 5.6,
+        sizeMl = 330,
+        available = true,
+        thumbnailUrl = URI("https://cdn.shopify.com/s/files/1/0060/1574/6161/products/CB-Assets-Can-Master-640x625-330ml-Brockwell-F_large.png?v=1539104364"),
+        url = URI("https://shop.canopybeer.com/products/brockwell-ipa")
+      ),
+      ITEMS.first { it.name == "Brockwell IPA" }
+    )
+  }
+
+  @Test
+  fun `identifies sold out`() {
+    assertFalse(ITEMS.first { it.name == "Champion Kölsch" }.available)
+  }
+
+  @Test
+  fun `excludes packs`() {
+    assertTrue(ITEMS.none { it.name.contains("box|pack".toRegex(IGNORE_CASE)) })
+  }
+
+
+}
+
