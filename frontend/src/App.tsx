04227c81b11565a7b6b936d4fa3e960c32bc468e
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-06-24 19:26:48 +0100

    WIP

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,41 +5,81 @@ import { Inventory, Item } from "./model";
 import "./index.css";
 
 const App = () => (
-  <div>
-    <SortableTable data={(inventory as Inventory).items}>
-      <Column
-        name="Brewery"
-        render={renderBrewery}
-        selector={(item) => item.brewery}
-      />
-      <Column
-        className="thumbnail"
-        render={renderThumbnail}
-      />
-      <Column
-        name="Name"
-        className="name"
-        render={renderName}
-        selector={(item) => item.name}
-      />
-      <Column
-        name="ABV"
-        render={renderAbv}
-        selector={(item) => item.abv}
-      />
-      <Column
-        name="Size"
-        className="size"
-        render={renderSize}
-        selector={(item) => item.sizeMl}
-      />
-      <Column
-        name="Price per item"
-        render={renderPrice}
-        selector={(item) => item.perItemPrice}
-      />
-    </SortableTable>
+  <div style={{ display: "flex" }}>
+    <div id="left-gutter">
+      <Settings inventory={inventory as Inventory} />
+    </div>
+
+    <InventoryTable inventory={inventory as Inventory} />
+
+    <div id="right-gutter" style={{ margin: "20px", border: "1px" }}>
+    </div>
   </div>
+);
+
+interface SettingsTableProps {
+  inventory: Inventory;
+}
+
+
+const Settings = (props: SettingsTableProps) => {
+  const breweries = Array.from(new Set(props.inventory.items.map(item => item.brewery)));
+
+  return (
+    <div className="settings">
+      <h4>Filter breweries</h4>
+      {
+        breweries.map((brewery, idx) => (
+          <label key={idx} className="selectable">
+            {brewery}
+            <input type="checkbox" checked={true} />
+            <span className="checkmark"></span>
+          </label>
+        ))
+      } 
+    </div>
+  );
+};
+
+
+interface InventoryTableProps {
+  inventory: Inventory;
+}
+
+const InventoryTable = (props: InventoryTableProps) => (
+  <SortableTable data={inventory.items}>
+    <Column
+      name="Brewery"
+      render={renderBrewery}
+      selector={(item) => item.brewery}
+    />
+    <Column
+      className="thumbnail"
+      render={renderThumbnail}
+    />
+    <Column
+      name="Name"
+      className="name"
+      render={renderName}
+      selector={(item) => item.name}
+    />
+    <Column
+      name="ABV"
+      render={renderAbv}
+      selector={(item) => item.abv}
+    />
+    <Column
+      name="Size"
+      className="size"
+      render={renderSize}
+      selector={(item) => item.sizeMl}
+    />
+    <Column
+      name="Price per item"
+      render={renderPrice}
+      selector={(item) => item.perItemPrice}
+    />
+  </SortableTable>
 );
 
 const renderBrewery = (item: Item) => item.brewery;
