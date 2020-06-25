e0c0e706459b71cc1e1768221cf78edc169569c4
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-06-25 16:50:29 +0100

    WIP

--- src/main/kotlin/choliver/neapi/Scraper.kt
+++ src/main/kotlin/choliver/neapi/Scraper.kt
@@ -3,9 +3,11 @@ package choliver.neapi
 import org.jsoup.nodes.Document
 import java.net.URI
 
-interface Scraper {
+interface Scraper<T> {
   val name: String
   fun Context.scrape(): List<ScrapedItem>
+
+  fun Context.scrapeFoSho(): List<Pair<URI, (Document) -> ScrapedItem>> = emptyList()
 
   interface Context {
     fun request(url: URI): Document
