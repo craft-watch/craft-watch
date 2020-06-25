8d58ea4876613b6cd9b9b16da763d232f1c1f3de
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-06-25 20:34:11 +0100

    Application (#53)
    * Create Application.kt
    
    * Run application in CI

--- src/main/kotlin/choliver/neapi/Application.kt
+++ src/main/kotlin/choliver/neapi/Application.kt
@@ -3,14 +3,13 @@ package choliver.neapi
 import choliver.neapi.scrapers.*
 import com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import java.io.File
 
 fun main() {
   val getter = HttpGetter(CACHE_DIR)
   val executor = Executor(getter)
   val mapper = jacksonObjectMapper().enable(INDENT_OUTPUT)
 
-  File("frontend/src/inventory.json").outputStream().use { ostream ->
+  INVENTORY_JSON_FILE.outputStream().use { ostream ->
     mapper.writeValue(ostream, executor.scrapeAll(
       BoxcarScraper(),
       CanopyScraper(),
