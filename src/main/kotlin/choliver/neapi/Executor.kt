2ba93140b5cba96668476ec9e5b3d5330737037e
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-06-26 11:27:09 +0100

    Improve logging

--- src/main/kotlin/choliver/neapi/Executor.kt
+++ src/main/kotlin/choliver/neapi/Executor.kt
@@ -9,49 +9,52 @@ class Executor(getter: HttpGetter) {
   private val logger = KotlinLogging.logger {}
 
   fun scrapeAll(vararg scrapers: Scraper) = Inventory(
-    items = scrapers.flatMap { scraper -> scrapeBrewery(scraper) }
+    items = scrapers.flatMap { ScraperExecutor(it).execute() }
   )
 
-  private fun scrapeBrewery(scraper: Scraper): List<Item> {
-    val brewery = scraper.name
-    logger.info("Scraping brewery: ${brewery}")
+  inner class ScraperExecutor(private val scraper: Scraper) {
+    private val brewery = scraper.name
 
-    return scrapeIndexSafely(scraper, brewery)
-      .mapNotNull { scrapeItem(it, brewery) }
-      .bestPricedItems()
-  }
+    fun execute(): List<Item> {
+      logger.info("[${brewery}] Scraping brewery")
 
-  private fun scrapeItem(entry: IndexEntry, brewery: String): Item? {
-    logger.info("Scraping item: ${entry.rawName}")
+      return scrapeIndexSafely(scraper)
+        .mapNotNull { scrapeItem(it) }
+        .bestPricedItems()
+    }
 
-    return when (val result = scrapeItemSafely(entry)) {
-      is Result.Item -> result.normalise(brewery, entry.url)
-      is Result.Skipped -> {
-        logger.info("Skipping because: ${result.reason}")
-        null
+    private fun scrapeItem(entry: IndexEntry): Item? {
+      logger.info("[${brewery}] Scraping item: ${entry.rawName}")
+
+      return when (val result = scrapeItemSafely(entry)) {
+        is Result.Item -> result.normalise(brewery, entry.url)
+        is Result.Skipped -> {
+          logger.info("[${brewery}] Skipping item because: ${result.reason}")
+          null
+        }
       }
     }
-  }
 
-  private fun scrapeIndexSafely(scraper: Scraper, brewery: String): List<IndexEntry> = try {
-    scraper.scrapeIndex(jsonGetter.request(scraper.rootUrl))
-  } catch (e: Exception) {
-    logger.error("Error scraping brewery: ${brewery}", e)
-    emptyList()
-  }
-
-  private fun scrapeItemSafely(entry: IndexEntry) = try {
-    entry.scrapeItem(jsonGetter.request(entry.url))
-  } catch (e: Exception) {
-    logger.error("Error scraping item: ${entry.rawName}", e)
-    Result.Skipped("Scraping error")
-  }
-
-  private fun List<Item>.bestPricedItems() = groupBy { it.name to it.summary }
-    .map { (key, group) ->
-      if (group.size > 1) {
-        logger.info("Eliminating ${group.size - 1} item(s) with worse prices for: ${key}")
-      }
-      group.minBy { it.perItemPrice }!!
+    private fun scrapeIndexSafely(scraper: Scraper): List<IndexEntry> = try {
+      scraper.scrapeIndex(jsonGetter.request(scraper.rootUrl))
+    } catch (e: Exception) {
+      logger.error("[${brewery}] Error scraping brewery", e)
+      emptyList()
     }
+
+    private fun scrapeItemSafely(entry: IndexEntry) = try {
+      entry.scrapeItem(jsonGetter.request(entry.url))
+    } catch (e: Exception) {
+      logger.error("[${brewery}] Error scraping item: ${entry.rawName}", e)
+      Result.Skipped("Error scraping item")
+    }
+
+    private fun List<Item>.bestPricedItems() = groupBy { it.name to it.summary }
+      .map { (key, group) ->
+        if (group.size > 1) {
+          logger.info("[${brewery}] Eliminating ${group.size - 1} more expensive item(s) for: ${key}")
+        }
+        group.minBy { it.perItemPrice }!!
+      }
+  }
 }
