dafa894f9d21e6484694639d37e2eafe3bc31019
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-06-27 20:29:57 +0100

    Clean up

--- src/test/kotlin/choliver/neapi/scrapers/MarbleScraperTest.kt
+++ src/test/kotlin/choliver/neapi/scrapers/MarbleScraperTest.kt
@@ -1,7 +1,11 @@
 package choliver.neapi.scrapers
 
+import choliver.neapi.Scraper
 import choliver.neapi.executeScraper
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Test
+import java.net.URI
 
 class MarbleScraperTest {
   companion object {
@@ -10,8 +14,50 @@ class MarbleScraperTest {
 
   @Test
   fun `finds all the beers`() {
-    ITEMS.forEach(::println)
-//    assertEquals(7, ITEMS.size)
+    assertEquals(28, ITEMS.size)
+  }
+
+  @Test
+  fun `extracts beer details`() {
+    assertEquals(
+      Scraper.Item(
+        name = "Earl Grey IPA",
+        summary = "IPA",
+        sizeMl = 500,
+        abv = 6.8,
+        perItemPrice = 4.00,
+        available = true,
+        thumbnailUrl = URI("https://marblebeers.com/wp-content/uploads/2017/11/Earl-Grey-500ml-Can-234x300.png")
+      ),
+      ITEMS.first { it.name == "Earl Grey IPA" }
+    )
+  }
+
+  @Test
+  fun `identifies mixed cases`() {
+    val cases = ITEMS.filter { it.mixed }
+
+    assertFalse(cases.isEmpty())
+    assertFalse(cases.any { it.summary != null })
+  }
+
+  @Test
+  fun `identifies kegs`() {
+    val kegs = ITEMS.filter { it.keg }
+
+    assertFalse(kegs.isEmpty())
+    assertFalse(kegs.any { it.name.contains("mini", ignoreCase = true) })
+  }
+
+  @Test
+  fun `identifies sold out`() {
+    assertFalse(ITEMS.first { it.name == "Cross Collar Half" }.available)
+  }
+
+  @Test
+  fun `removes irritating suffixes`() {
+    assertFalse(ITEMS.any { it.name.contains("cans", ignoreCase = true) })
+    assertFalse(ITEMS.any { it.name.contains("\\d+$".toRegex()) })
   }
 }
 
