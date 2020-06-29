1fcf1e7147875bf05a20f6cb98d7bc626af3c0bc
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-06-29 17:12:49 +0100

    Enable tests to run on specific historic data from cache

--- src/test/kotlin/watch/craft/TestUtils.kt
+++ src/test/kotlin/watch/craft/TestUtils.kt
@@ -4,33 +4,39 @@ import mu.KotlinLogging
 import watch.craft.getters.CachingGetter
 import watch.craft.getters.HtmlGetter
 import watch.craft.getters.HttpGetter
-import watch.craft.storage.GcsObjectStore
-import watch.craft.storage.LocalObjectStore
-import watch.craft.storage.StoreStructure
-import watch.craft.storage.WriteThroughObjectStore
-import java.time.Instant
+import watch.craft.storage.*
+import java.time.LocalDate
+import java.time.ZoneOffset
 
 private val logger = KotlinLogging.logger {}
 
+private val GOLDEN_TIMESTAMP = LocalDate.parse("2020-06-28")
+  .atStartOfDay(ZoneOffset.UTC).toInstant()
+
 fun executeScraper(scraper: Scraper): List<Scraper.Item> {
-  val store = WriteThroughObjectStore(
-    firstLevel = LocalObjectStore(CACHE_DIR),
-    secondLevel = GcsObjectStore(GCS_BUCKET)
+  // TODO - make this configurable
+  val store = ReadOnlyObjectStore(
+    WriteThroughObjectStore(
+      firstLevel = LocalObjectStore(CACHE_DIR),
+      secondLevel = GcsObjectStore(GCS_BUCKET)
+    )
   )
-  val structure = StoreStructure(store, Instant.now())
+  val structure = StoreStructure(store, GOLDEN_TIMESTAMP)
   val cachingGetter = CachingGetter(structure.cache, HttpGetter())
   val getter = HtmlGetter(cachingGetter)
   return scraper.scrapeIndex(getter.request(scraper.rootUrl))
     .mapNotNull {
       try {
         it.scrapeItem(getter.request(it.url))
+      } catch (e: SkipItemException) {
+        logger.info("Skipped because: ${e.message}")
+        null
       } catch (e: NonFatalScraperException) {
         logger.warn("Non-fatal exception: ${e.message}")
         null
       }
     }
 }
-
 
 fun List<Scraper.Item>.byName(name: String) = first { it.name == name }
 
