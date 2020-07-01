6a50bcf795bb3c057002bae93f9b7350f22e3781
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-07-02 00:23:54 +0100

    WIP

--- frontend/utils/strings.ts
+++ frontend/utils/strings.ts
@@ -1,4 +1,4 @@
-export const OUT_OF_STOCK = "Out of stock";
 export const REGULAR = "Regular";
 export const MINIKEG = "Minikeg";
 export const MIXED_CASE = "Mixed case";
+export const OUT_OF_STOCK = "Out of stock";
