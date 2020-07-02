55be69e95f83e99690d5be23223ae4e1ba89eeaa
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-07-02 10:43:14 +0100

    Make deploy message betetr

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -eu
 
+MESSAGE=$(git show -s --format=%B | head -n1 | sed -re 's/[(]#.*[)]//')
+
 git config --global user.email "bot@oliver-charlesworth.github.io"
 git config --global user.name "CircleCI Bot"
 
@@ -9,5 +11,5 @@ cd website
 git rm -r .
 cp -r ../frontend/out/. .
 git add .
-git commit --allow-empty -m "Update from ${CIRCLE_SHA1} (build URL: ${CIRCLE_BUILD_URL})"
+git commit --allow-empty -m "Deployed: ${MESSAGE} (build URL: ${CIRCLE_BUILD_URL})"
 git push
