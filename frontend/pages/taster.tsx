277fec2a49eaa564b216c9dbd086d5ba5b959709
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-07-04 21:36:41 +0100

    Fair taster menu (#123)
    * Implement fair taster menu
    
    * Cleanup
    
    * Cleaner implementation

--- frontend/pages/taster.tsx
+++ frontend/pages/taster.tsx
@@ -5,26 +5,15 @@ import App from "../components/App";
 import { Item } from "../utils/model";
 import { items as inventoryItems, capturedAt, categories } from "../utils/inventory";
 
+const TASTER_MENU_SIZE = 20;
+
 const ThisPage = (): JSX.Element => {
   const [items, setItems] = useState<Array<Item>>([]);
 
   // TODO - is there a better way to avoid this being captured by SSG?
   useEffect(() => {
-    const sample = _.sample(_.filter(inventoryItems, item => !item.keg && !item.mixed && item.available), 20);
-
-    // Sorted, but then brewery order is randomised
-    setItems(
-      _.flatten(
-        _.values(
-          _.shuffle(
-            _.groupBy(
-              _.sortBy(sample, item => item.name),
-              item => item.brewery
-            )
-          )
-        )
-      )
-    );
+    const sample = generateFairTasterMenu(inventoryItems);
+    setItems(_.sortBy(_.sortBy(sample, item => item.name), item => item.brewery));
   }, []);
 
   return (
@@ -35,6 +24,37 @@ const ThisPage = (): JSX.Element => {
       <App capturedAt={capturedAt} items={items} categories={categories} />
     </Page>
   );
+};
+
+// Avoid over-representing breweries that have a ton of beers.
+const generateFairTasterMenu = (items: Array<Item>): Array<Item> => {
+  // Remove inappropriate items for a taster menu
+  const relevant = _.filter(items, item => !item.keg && !item.mixed && item.available);
+
+  const byBrewery = _.groupBy(relevant, item => item.brewery);
+
+  // Pick breweries with *almost* uniform distribution.
+  // We allow breweries with lots of beers to feature *slightly* more.
+  const breweryPicks = _.shuffle(
+    _.flatten(
+      _.map(byBrewery, (items, brewery) => {
+        const count = _.size(items);
+        const rep =
+          (count >= 10) ? 6 :
+          (count >= 5) ? 5 :
+          4;
+        return _.times(rep, () => brewery);
+      })
+    )
+  );
+
+  // TODO - what if TASTER_MENU_SIZE < num items?
+  const picked = new Set<Item>();
+  let idx = 0;
+  while (picked.size < TASTER_MENU_SIZE) {
+    picked.add(_.sample(byBrewery[breweryPicks[idx++]]));
+  }
+  return Array.from(picked);
 };
 
 export default ThisPage;
