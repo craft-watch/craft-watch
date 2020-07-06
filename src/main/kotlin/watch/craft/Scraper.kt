5a27a1c993522d633feb58370c4a21e3ff1f68a2
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-07-06 13:30:59 +0100

    WIP

--- src/main/kotlin/watch/craft/Scraper.kt
+++ src/main/kotlin/watch/craft/Scraper.kt
@@ -1,6 +1,8 @@
 package watch.craft
 
 import org.jsoup.nodes.Document
+import watch.craft.Scraper.Job.Leaf
+import watch.craft.Scraper.Job.More
 import java.net.URI
 
 interface Scraper {
@@ -8,6 +10,29 @@ interface Scraper {
   val rootUrls: List<URI>
 
   fun scrapeIndex(root: Document): List<IndexEntry>
+
+  // TODO - eliminate this
+  val rootJobs: List<Job> get() = rootUrls
+    .map { url ->
+      More(url = url) { doc ->
+        scrapeIndex(doc).map { entry ->
+          Leaf(rawName = entry.rawName, url = entry.url, work = entry.scrapeItem)
+        }
+      }
+    }
+
+  sealed class Job {
+    data class More(
+      val url: URI,
+      val work: (doc: Document) -> List<Job>
+    ) : Job()
+
+    data class Leaf(
+      val rawName: String,
+      val url: URI,
+      val work: (doc: Document) -> ScrapedItem
+    ) : Job()
+  }
 
   data class IndexEntry(
     val rawName: String,
