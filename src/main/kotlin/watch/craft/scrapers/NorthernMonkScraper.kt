855c36e4cd27dacd4c7f5b26100ec22381434a82
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-07-06 20:51:05 +0100

    Tidy

--- src/main/kotlin/watch/craft/scrapers/NorthernMonkScraper.kt
+++ src/main/kotlin/watch/craft/scrapers/NorthernMonkScraper.kt
@@ -2,8 +2,9 @@ package watch.craft.scrapers
 
 import org.jsoup.nodes.Document
 import watch.craft.*
-import watch.craft.Scraper.*
+import watch.craft.Scraper.Job
 import watch.craft.Scraper.Job.Leaf
+import watch.craft.Scraper.ScrapedItem
 import java.net.URI
 import kotlin.text.RegexOption.IGNORE_CASE
 
