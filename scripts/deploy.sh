20d161466ac192c39c4165c0a1a78daef73fcc4d
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-07-07 15:30:14 +0100

    WIP

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,15 +6,6 @@ MESSAGE=$(git show -s --format=%B | head -n1 | sed -re 's/[(]#.*[)]//')
 git config --global user.email "bot@craft-watch.github.io"
 git config --global user.name "Deploy Bot"
 
-echo "PWD"
-pwd
-
-echo "LS HERE"
-ls -l
-
-echo "LS ONE UP"
-ls -l ../
-
 git clone --depth 1 git@github.com:craft-watch/craft-watch.github.io.git website
 cd website
 git rm -r .
