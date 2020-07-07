40e6afa035802eaca07a5042df184c606af6521c
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-07-07 15:25:51 +0100

    WIP

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,12 +4,21 @@ set -eu
 MESSAGE=$(git show -s --format=%B | head -n1 | sed -re 's/[(]#.*[)]//')
 
 git config --global user.email "bot@craft-watch.github.io"
-git config --global user.name "CircleCI Bot"
+git config --global user.name "Deploy Bot"
+
+echo "PWD"
+pwd
+
+echo "LS HERE"
+ls -l
+
+echo "LS ONE UP"
+ls -l ../
 
 git clone --depth 1 git@github.com:craft-watch/craft-watch.github.io.git website
 cd website
 git rm -r .
 cp -r ../frontend/out/. .
 git add .
-git commit --allow-empty -m "Deployed: ${MESSAGE} (build URL: ${CIRCLE_BUILD_URL})"
+git commit --allow-empty -m "Deployed: ${MESSAGE} (run ID: ${GITHUB_RUN_ID})"
 git push
