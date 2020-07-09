97dfd02d834cbf8de6b0f749c2de1988cbc1376b
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-07-09 20:05:13 +0100

    Add Hackney Church

---
+++ src/test/kotlin/watch/craft/scrapers/HackneyChurchScraperTest.kt
@@ -0,0 +1,54 @@
+package watch.craft.scrapers
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import watch.craft.*
+import watch.craft.Scraper.ScrapedItem
+import java.net.URI
+
+class HackneyChurchScraperTest {
+  companion object {
+    private val ITEMS = executeScraper(HackneyChurchScraper(), dateString = "2020-07-09")
+  }
+
+  @Test
+  fun `finds all the beers`() {
+//    ITEMS.display()
+    assertEquals(6, ITEMS.size)
+  }
+
+  @Test
+  fun `extracts beer details`() {
+    assertEquals(
+      ScrapedItem(
+        name = "Superfly IPA",
+        abv = null,   // Not listed
+        offers = setOf(
+          Offer(totalPrice = 36.00, quantity = 12, sizeMl = 330)
+        ),
+        available = true,
+        thumbnailUrl = URI("https://cdn.shopify.com/s/files/1/0267/3842/5904/products/IMG_26312copy_200x.png?v=1593030329")
+      ),
+      ITEMS.byName("Superfly IPA").noDesc()
+    )
+  }
+
+  @Test
+  fun `extracts description`() {
+    assertNotNull(ITEMS.byName("Superfly IPA").desc)
+  }
+
+  @Test
+  fun `identifies mixed packs`() {
+    val item = ITEMS.byName("Mixed Case")
+
+    assertTrue(item.mixed)
+    assertTrue(item.onlyOffer().quantity > 1)
+  }
+
+  @Test
+  fun `identifies sold out`() {
+    assertFalse(ITEMS.byName("Session with Ry IPA").available)
+  }
+}
+
