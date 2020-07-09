d62b1a6c23469061c70211539bde7e372c85b970
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-07-09 11:37:00 +0100

    Update to new scraper API

--- src/main/kotlin/watch/craft/scrapers/RedWillowScraper.kt
+++ src/main/kotlin/watch/craft/scrapers/RedWillowScraper.kt
@@ -1,38 +1,48 @@
 package watch.craft.scrapers
 
-import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
-import watch.craft.*
-import watch.craft.Scraper.IndexEntry
+import watch.craft.Brewery
+import watch.craft.Scraper
+import watch.craft.Scraper.Job.Leaf
 import watch.craft.Scraper.ScrapedItem
+import watch.craft.utils.*
 import java.net.URI
 
 class RedWillowScraper : Scraper {
-  override val name = "Red Willow"
-  override val rootUrls = listOf(URI("https://www.redwillowbrewery.com/shop"))
+  override val brewery = Brewery(
+    shortName = "Red Willow",
+    name = "Red Willow Brewery",
+    location = "Macclesfield, Cheshire",
+    websiteUrl = URI("https://www.redwillowbrewery.com/")
+  )
 
-  override fun scrapeIndex(root: Document) = root
-    .selectMultipleFrom(".ProductList-grid .ProductList-item")
-    .map { el ->
-      val rawName = el.textFrom(".ProductList-title")
+  override val jobs = forRootUrls(ROOT_URL) { root ->
+    root
+      .selectMultipleFrom(".ProductList-grid .ProductList-item")
+      .map { el ->
+        val rawName = el.textFrom(".ProductList-title")
 
-      IndexEntry(rawName, el.hrefFrom("a.ProductList-item-link")) { doc ->
+        Leaf(rawName, el.hrefFrom("a.ProductList-item-link")) { doc ->
 
-        val parts = rawName.extract("([^0-9]+)\\s+(\\d(\\.\\d+)?)%")//\\s(.*?)")
-        println(parts)
+          val parts = rawName.extract("([^0-9]+)\\s+(\\d(\\.\\d+)?)%")//\\s(.*?)")
+          println(parts)
 
-        ScrapedItem(
-          name = rawName,
-          summary = null,
-          desc = doc.normaliseParagraphsFrom(".ProductItem-details-excerpt"),
-          sizeMl = null,
-          abv = null,
-          available = true,
-          numItems = 1,
-          price = 0.00,
-          thumbnailUrl = el.dataSrcFrom("img.ProductList-image")
-        )
+          ScrapedItem(
+            name = rawName,
+            summary = null,
+            desc = doc.formattedTextFrom(".ProductItem-details-excerpt"),
+            sizeMl = null,
+            abv = null,
+            available = true,
+            numItems = 1,
+            price = 0.00,
+            thumbnailUrl = el.dataSrcFrom("img.ProductList-image")
+          )
+        }
       }
-    }
+  }
+
+  companion object {
+    private val ROOT_URL = URI("https://www.redwillowbrewery.com/shop")
+  }
 
 }
