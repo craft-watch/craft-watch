384f32c387ca1bcb493a0c4f03be8149f622b48f
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-07-09 15:11:59 +0100

    WIP

--- src/main/kotlin/watch/craft/scrapers/RedWillowScraper.kt
+++ src/main/kotlin/watch/craft/scrapers/RedWillowScraper.kt
@@ -6,6 +6,7 @@ import watch.craft.Brewery
 import watch.craft.Scraper
 import watch.craft.Scraper.Job.Leaf
 import watch.craft.Scraper.ScrapedItem
+import watch.craft.SkipItemException
 import watch.craft.utils.*
 import java.net.URI
 
@@ -17,6 +18,9 @@ class RedWillowScraper : Scraper {
     websiteUrl = URI("https://www.redwillowbrewery.com/")
   )
 
+  // TODO - extract summary
+  // TODO - mixed
+
   override val jobs = forRootUrls(ROOT_URL) { root ->
     root
       .selectMultipleFrom(".ProductList-grid .ProductList-item")
@@ -24,51 +28,44 @@ class RedWillowScraper : Scraper {
         val rawName = el.textFrom(".ProductList-title")
 
         Leaf(rawName, el.hrefFrom("a.ProductList-item-link")) { doc ->
-//          val data = doc
-//            .selectMultipleFrom("script[type=application/ld+json]")
-//            .map { it.jsonFrom<Map<String, Any>>() }
-//            .forEach { println(mapper().writeValueAsString(it)) }
-
-
-          doc.maybe { attrFrom(".product-variants", "data-variants") }
-
           val bestDeal = doc.extractBestDeal()
 
-//          val parts = rawName.extract("([^0-9]+)\\s+(\\d(\\.\\d+)?)%")//\\s(.*?)")
-//          println(parts)
+          val desc = doc.formattedTextFrom(".ProductItem-details-excerpt")
+          val allText = rawName + "\n" + desc
+
+          if (BLACKLIST.any { rawName.contains(it, ignoreCase = true) }) {
+            throw SkipItemException("Identified as non-beer")
+          }
 
           ScrapedItem(
-            name = rawName.extract("^[A-Za-z-\\s]+")[0].trim().toTitleCase(),
+            name = rawName.extract("^[A-Za-z-'\\s]+")[0].trim().toTitleCase(),
             summary = null,
-            desc = doc.formattedTextFrom(".ProductItem-details-excerpt"),
-            sizeMl = null,
+            desc = desc,
+            mixed = rawName.contains("mixed", ignoreCase = true),
+            sizeMl = allText.maybe { sizeMlFrom() },
             abv = rawName.maybe { abvFrom() },
             available = true,
             numItems = bestDeal.numItems,
             price = bestDeal.price,
-            thumbnailUrl = el.dataSrcFrom("img.ProductList-image")
+            thumbnailUrl = doc.extractSmallThumbnail()
           )
         }
       }
   }
 
+  // By default, the images are huge!
+  private fun Document.extractSmallThumbnail() = (
+    dataSrcFrom("img.ProductItem-gallery-slides-item-image").toString() + "?format=200w"
+    ).toUri()
 
-  private fun Document.extractBestDeal(): Deal {
-    val variants = maybe {
-      attrFrom(".product-variants", "data-variants").parseJson<List<Variant>>()
-    }
-
-    return if (variants != null) {
-      variants
-        .map { Deal(price = it.price.toDouble() / 100, numItems = it.attributes.quantity) }
-        .minBy { it.price / it.numItems }!!
-    } else {
-      Deal(
-        price = priceFrom(".product-price"),
-        numItems = extractFrom(".ProductItem-details-title", "(\\d+)\\s*x").intFrom(1)
-      )
-    }
-  }
+  private fun Document.extractBestDeal() = maybe { attrFrom(".product-variants", "data-variants") }
+    ?.parseJson<List<Variant>>()
+    ?.map { Deal(price = it.price.toDouble() / 100, numItems = it.attributes.quantity) }
+    ?.minBy { it.price / it.numItems }
+    ?: Deal(
+      price = priceFrom(".product-price"),
+      numItems = extractFrom(".ProductItem-details-title", "(\\d+)\\s*x").intFrom(1)
+    ) // Fallback to assuming a single price
 
   private data class Deal(
     val price: Double,
@@ -87,5 +84,7 @@ class RedWillowScraper : Scraper {
 
   companion object {
     private val ROOT_URL = URI("https://www.redwillowbrewery.com/shop")
+
+    private val BLACKLIST = listOf("glassware")
   }
 }
