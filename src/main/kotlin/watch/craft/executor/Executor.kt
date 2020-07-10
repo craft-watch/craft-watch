963d0651b15d3308d5222c1d258881fca691935a
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-07-10 15:37:37 +0100

    WIP

--- src/main/kotlin/watch/craft/executor/Executor.kt
+++ src/main/kotlin/watch/craft/executor/Executor.kt
@@ -74,26 +74,30 @@ class Executor(
           logger.info("[${key.brewery}] Merging ${group.size} item(s) for [${key.name}]")
         }
 
-        // Prefer to use a non-keg for description / thumbnail, etc.
-        val archetype = group.firstOrNull { item -> item.offers.none { it.keg } }
-          ?: group.first()
+        val offers = group.mergeAndPrioritiseOffers()
 
-        // TODO - need a stable notion of "first" - will need to sort upstream
-        // TODO - fill in missing fields from non-archetypes
-        // TODO - do we want a URL per offer?  Keg vs. can vs. item may be different pages
+        val headlineItem = offers.first().item
 
-        val allOffers = group
-          .flatMap { it.offers }
-          .distinctBy { round(it.pricePerMl() * 100) } // Work in pence to avoid FP precision issues
-          .sortedWith(compareBy(
-            { it.keg },   // Kegs should be lowest priority
-            { it.pricePerMl() },
-            { it.quantity } // All being equal, we prefer to buy fewer cans
-          ))
-
-        archetype.copy(offers = allOffers)
+        headlineItem.copy(offers = offers.map { it.offer })
       }
     )
+
+  // TODO - need a stable notion of "first" - will need to sort upstream
+  // TODO - fill in missing fields from non-archetypes
+  // TODO - do we want a URL per offer?  Keg vs. can vs. item may be different pages
+  private fun List<Item>.mergeAndPrioritiseOffers() =
+    flatMap { item -> item.offers.map { ItemAndOffer(item, it) } }
+      .distinctBy { round(it.offer.pricePerMl() * 100) } // Work in pence to avoid FP precision issues
+      .sortedWith(compareBy(
+        { it.offer.keg },   // Kegs should be lowest priority
+        { it.offer.pricePerMl() },
+        { it.offer.quantity } // All being equal, we prefer to buy fewer cans
+      ))
+
+  private data class ItemAndOffer(
+    val item: Item,
+    val offer: Offer
+  )
 
   private fun Inventory.sortItems() = copy(items = items
     .sortedWith(compareBy(
