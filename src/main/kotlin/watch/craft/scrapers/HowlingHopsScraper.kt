5e1a36389f9ade603ffa29d237462c2804fe2f4f
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-07-11 20:22:34 +0100

    Begin implementing common JSON-LD behaviour

--- src/main/kotlin/watch/craft/scrapers/HowlingHopsScraper.kt
+++ src/main/kotlin/watch/craft/scrapers/HowlingHopsScraper.kt
@@ -31,7 +31,7 @@ class HowlingHopsScraper : Scraper {
           val desc = doc.textFrom(".woocommerce-product-details__short-description")
           val parts = extractVariableParts(desc)
 
-          val data = doc.jsonFrom<Data>("script[type=application/ld+json]")
+          val data = doc.jsonLdFrom<Data>("script[type=application/ld+json]")
           val product = data.graph.filterIsInstance<Product>().single()
           val available = product.offers.any { it.availability == "http://schema.org/InStock" }
 
