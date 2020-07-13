cc56ac214dc4f99da8381e4e58167ab68095cf25
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-07-13 15:06:04 +0100

    WIP

--- src/main/kotlin/watch/craft/scrapers/ForestRoadScraper.kt
+++ src/main/kotlin/watch/craft/scrapers/ForestRoadScraper.kt
@@ -1,6 +1,8 @@
 package watch.craft.scrapers
 
 import watch.craft.Brewery
+import watch.craft.Format.BOTTLE
+import watch.craft.Format.CAN
 import watch.craft.Offer
 import watch.craft.Scraper
 import watch.craft.Scraper.Job.Leaf
@@ -8,6 +10,7 @@ import watch.craft.Scraper.ScrapedItem
 import watch.craft.SkipItemException
 import watch.craft.utils.*
 import java.net.URI
+import kotlin.text.RegexOption.IGNORE_CASE
 
 class ForestRoadScraper : Scraper {
   override val brewery = Brewery(
@@ -30,23 +33,28 @@ class ForestRoadScraper : Scraper {
             throw SkipItemException("Subscriptions aren't something we can model")
           }
 
-          val desc = doc.formattedTextFrom(".ProductItem-details-excerpt")
-
+          val desc = doc.formattedTextFrom(".ProductItem-details-excerpt").toTitleCase()
           val mixed = title.contains("mixed", ignoreCase = true)
 
+          val name = title
+            .replace("[(].*[)]".toRegex(), "")
+            .replace("cans".toRegex(IGNORE_CASE), "")
+            .trim()
+            .toTitleCase()
 
           ScrapedItem(
-            name = title.toTitleCase(),
-            summary = null,
+            name = name,
+            summary = desc.split("\n")[0],
             desc = desc,
             mixed = true,
             abv = if (mixed) null else desc.abvFrom(),
             available = true,
             offers = setOf(
               Offer(
-                quantity = 1,
+                quantity = title.maybe { extract("(\\d+)\\s*x").intFrom(1) } ?: 1,
                 totalPrice = el.priceFrom(".product-price"),
-                sizeMl = null
+                sizeMl = title.sizeMlFrom(),
+                format = if (title.contains("cans", ignoreCase = true)) CAN else BOTTLE
               )
             ),
             thumbnailUrl = el.dataSrcFrom("img.ProductList-image")
