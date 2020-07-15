9b04bd4fe724dcc9aa2b6640ab3e299e444d8abd
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-07-15 09:25:39 +0100

    Move scraper list to Scrapers.kt

---
+++ src/main/kotlin/watch/craft/Scrapers.kt
@@ -0,0 +1,39 @@
+package watch.craft
+
+import watch.craft.scrapers.*
+
+val SCRAPERS = listOf(
+  BeakScraper(),
+  BoxcarScraper(),
+  CanopyScraper(),
+  CloudwaterScraper(),
+  DeyaScraper(),
+  FivePointsScraper(),
+  ForestRoadScraper(),
+  FourpureScraper(),
+  GipsyHillScraper(),
+  InnisAndGunnScraper(),
+  JeffersonsScraper(),
+  HackneyChurchScraper(),
+  HowlingHopsScraper(),
+  MarbleScraper(),
+  NorthernMonkScraper(),
+  OrbitScraper(),
+  PadstowScraper(),
+  PillarsScraper(),
+  PollysScraper(),
+  PressureDropScraper(),
+  RedchurchScraper(),
+  RedWillowScraper(),
+  RoostersScraper(),
+  SirenScraper(),
+  SolvayScraper(),
+  StewartScraper(),
+  ThornbridgeScraper(),
+  UnityScraper(),
+  VerdantScraper(),
+  VillagesScraper(),
+  WanderScraper(),
+  WiperAndTrueScraper(),
+  WylamScraper()
+)
