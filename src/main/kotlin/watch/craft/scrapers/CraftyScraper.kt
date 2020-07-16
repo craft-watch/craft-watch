ffe42ac3edf39e03442d2ef78dd4dba58fa9a8ef
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-07-16 11:05:16 +0100

    Crafty Brewing (#234)
    * WIP
    
    * Finish the job

---
+++ src/main/kotlin/watch/craft/scrapers/CraftyScraper.kt
@@ -0,0 +1,87 @@
+package watch.craft.scrapers
+
+import com.fasterxml.jackson.annotation.JsonAlias
+import com.fasterxml.jackson.annotation.JsonProperty
+import org.jsoup.nodes.Document
+import watch.craft.Offer
+import watch.craft.Scraper
+import watch.craft.Scraper.Job.Leaf
+import watch.craft.Scraper.ScrapedItem
+import watch.craft.utils.*
+import java.net.URI
+
+class CraftyScraper : Scraper {
+  override val jobs = forPaginatedRootUrl(ROOT_URL) { root ->
+    root
+      .selectFrom("ul.products")  // Later sections are mostly overlapping
+      .selectMultipleFrom(".product")
+      .map { el ->
+        val title = el.textFrom(".woocommerce-loop-product__title")
+
+        Leaf(title, el.hrefFrom(".woocommerce-LoopProduct-link")) { doc ->
+          val mixed = doc.textFrom(".product_meta").contains("mixed", ignoreCase = true)
+          val desc = doc.formattedTextFrom(".et_pb_wc_description")
+
+          ScrapedItem(
+            name = title
+              .replace("[(].*[)]".toRegex(), "")
+              .replace("\\s–\\s.*".toRegex(), "")
+              .trim(),
+            summary = null, // No obviously-useful info to extract
+            desc = desc,
+            mixed = mixed,
+            abv = if (mixed) null else desc.abvFrom(),
+            available = ".woosticker_sold" !in el,
+            offers = doc.extractOffers(desc, mixed).toSet(),
+            thumbnailUrl = el.srcFrom(".attachment-woocommerce_thumbnail")
+          )
+        }
+      }
+  }
+
+  private fun Document.extractOffers(desc: String, mixed: Boolean): List<Offer> {
+    val sizeMl = desc.sizeMlFrom()
+    val format = desc.formatFrom()
+    return maybe { attrFrom(".variations_form", "data-product_variations") }
+      ?.jsonFrom<List<Variant>>()
+      ?.map {
+        Offer(
+          totalPrice = it.displayPrice,
+          quantity = it.attributes.packSize.extract("\\d+").intFrom(0),
+          sizeMl = sizeMl,
+          format = format
+        )
+      }
+      ?: listOf(
+        Offer(
+          totalPrice = priceFrom(".price"),
+          quantity = if (mixed) desc.quantityFrom() else 1,
+          sizeMl = sizeMl,
+          format = format
+        ) // Fallback to assuming a single price
+      )
+  }
+
+  private fun String.quantityFrom() = HARDCODED_QUANTITIES.entries.first { containsWord(it.key) }.value
+
+  private data class Variant(
+    @JsonProperty("display_price")
+    val displayPrice: Double,
+    val attributes: Attributes
+  ) {
+    data class Attributes(
+      @JsonProperty("attribute_pa_pack-size")
+      @JsonAlias("attribute_pa_small-pack-size")
+      val packSize: String
+    )
+  }
+
+  companion object {
+    val ROOT_URL = URI("https://www.craftybrewing.co.uk/crafty-core-beers/")
+
+    private val HARDCODED_QUANTITIES = mapOf(
+      "six" to 6,
+      "dozen" to 12
+    )
+  }
+}
---
+++ src/test/kotlin/watch/craft/scrapers/CraftyScraperTest.kt
@@ -0,0 +1,57 @@
+package watch.craft.scrapers
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import watch.craft.*
+import watch.craft.Format.BOTTLE
+import watch.craft.Scraper.ScrapedItem
+import java.net.URI
+
+class CraftyScraperTest {
+  companion object {
+    private val ITEMS = executeScraper(CraftyScraper(), dateString = "2020-07-15")
+  }
+
+  @Test
+  fun `finds all the beers`() {
+    assertEquals(17, ITEMS.size)
+  }
+
+  @Test
+  fun `extracts beer details`() {
+    assertEquals(
+      ScrapedItem(
+        name = "Heady Steady Go APA",
+        abv = 4.5,
+        offers = setOf(
+          Offer(totalPrice = 6.00, quantity = 6, sizeMl = 500, format = BOTTLE),
+          Offer(totalPrice = 12.00, quantity = 12, sizeMl = 500, format = BOTTLE),
+          Offer(totalPrice = 24.00, quantity = 24, sizeMl = 500, format = BOTTLE)
+        ),
+        available = true,
+        thumbnailUrl = URI("https://www.craftybrewing.co.uk/wp-content/uploads/2020/03/crafty-brewing-heady-steady-go-1-500x500.jpg")
+      ),
+      ITEMS.byName("Heady Steady Go APA").noDesc()
+    )
+  }
+
+  @Test
+  fun `extracts description`() {
+    assertNotNull(ITEMS.byName("Heady Steady Go APA").desc)
+  }
+
+  @Test
+  fun `identifies mixed`() {
+    val item = ITEMS.byName("Crafty Dozen")
+
+    assertTrue(item.mixed)
+    assertEquals(12, item.onlyOffer().quantity)
+    assertEquals(27.50, item.onlyOffer().totalPrice)
+  }
+
+  @Test
+  fun `identifies sold-out`() {
+    assertFalse(ITEMS.byName("Dunsfold Best Mild Ale").available)
+  }
+}
+
