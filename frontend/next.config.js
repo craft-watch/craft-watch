2f05fe6701a6201494ed5f4e0c716cacbfe5bb51
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-07-26 17:23:44 +0100

    Remove old garbage from next config (#280)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   reactStrictMode: true,
   poweredByHeader: false,
-  exportPathMap: function(defaultPathMap) {
-    return Object.assign(defaultPathMap, {
-      "/": { page: "/new" }
-    });
-  },
 }
