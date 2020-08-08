3d23941886279daedaaca5a40087d7adfefb696f
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-08-08 12:56:39 +0100

    Reimplement Gipsy Hill

--- src/test/kotlin/watch/craft/scrapers/GipsyHillScraperTest.kt
+++ src/test/kotlin/watch/craft/scrapers/GipsyHillScraperTest.kt
@@ -2,20 +2,18 @@ package watch.craft.scrapers
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import watch.craft.Offer
+import watch.craft.*
 import watch.craft.Scraper.Node.ScrapedItem
-import watch.craft.byName
-import watch.craft.executeScraper
-import watch.craft.noDesc
 import java.net.URI
 
 class GipsyHillScraperTest {
   companion object {
-    private val ITEMS = executeScraper(GipsyHillScraper())
+    private val ITEMS = executeScraper(GipsyHillScraper(), dateString = "2020-08-08")
   }
 
   @Test
   fun `finds all the beers`() {
+    ITEMS.display()
     assertEquals(20, ITEMS.size)
   }
 
@@ -26,29 +24,24 @@ class GipsyHillScraperTest {
         name = "Carver",
         summary = "Micro IPA",
         offers = setOf(
-          Offer(totalPrice = 2.20, sizeMl = 330)
+          Offer(totalPrice = 2.20, sizeMl = 330, format = Format.CAN)
         ),
         abv = 2.8,
         available = true,
-        thumbnailUrl = URI("https://i1.wp.com/gipsyhillbrew.com/wp-content/uploads/2018/11/CARVER.png?resize=300%2C300&ssl=1")
+        thumbnailUrl = URI("https://i2.wp.com/shop.gipsyhillbrew.com/wp-content/uploads/2020/04/CARVER.jpg?fit=644%2C800&#038;ssl=1")
       ),
       ITEMS.byName("Carver").noDesc()
     )
   }
 
   @Test
-  fun `identifies mixed cases`() {
-    assertTrue(ITEMS.byName("DJ BBQ Box").mixed)
-  }
-
-  @Test
-  fun `ignores abv for multi-pack`() {
-    assertNull(ITEMS.byName("DJ BBQ Box").abv)
-  }
-
-  @Test
   fun `extracts description`() {
     assertNotNull(ITEMS.byName("Carver").desc)
+  }
+
+  @Test
+  fun `identifies sold-out`() {
+    assertFalse(ITEMS.byName("Dinomania").available)
   }
 }
 
