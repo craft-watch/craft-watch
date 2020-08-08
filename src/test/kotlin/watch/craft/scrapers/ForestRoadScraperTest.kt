2a417516d847783132c68e0cb043e8231fb781b5
Oliver Charlesworth <57868158+oliver-charlesworth@users.noreply.github.com>
2020-08-08 13:43:36 +0100

    Fix Forest Road

--- src/test/kotlin/watch/craft/scrapers/ForestRoadScraperTest.kt
+++ src/test/kotlin/watch/craft/scrapers/ForestRoadScraperTest.kt
@@ -3,19 +3,20 @@ package watch.craft.scrapers
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import watch.craft.*
-import watch.craft.Format.BOTTLE
+import watch.craft.Format.CAN
 import watch.craft.Format.KEG
 import watch.craft.Scraper.Node.ScrapedItem
+import watch.craft.dsl.containsWord
 import java.net.URI
 
 class ForestRoadScraperTest {
   companion object {
-    private val ITEMS = executeScraper(ForestRoadScraper(), dateString = "2020-07-13")
+    private val ITEMS = executeScraper(ForestRoadScraper(), dateString = "2020-08-08")
   }
 
   @Test
   fun `finds all the beers`() {
-    assertEquals(10, ITEMS.size)
+    assertEquals(9, ITEMS.size)
   }
 
   @Test
@@ -23,15 +24,15 @@ class ForestRoadScraperTest {
     assertEquals(
       ScrapedItem(
         name = "Work IPA",
-        summary = "India Pale Ale",
         abv = 5.4,
         offers = setOf(
-          Offer(totalPrice = 26.00, quantity = 12, sizeMl = 330, format = BOTTLE)
+          Offer(totalPrice = 27.00, quantity = 12, sizeMl = 330, format = CAN),
+          Offer(totalPrice = 50.00, quantity = 24, sizeMl = 330, format = CAN)
         ),
         available = true,
-        thumbnailUrl = URI("https://static1.squarespace.com/static/5ca0c8b83560c3098a94e46b/5e70a00b24050371dc9942a8/5e70ac6f8e693374038081dd/1594035000018/")
+        thumbnailUrl = URI("https://cdn.shopify.com/s/files/1/0019/2226/9245/products/workcan_200x.jpg")
       ),
-      ITEMS.first { it.name == "Work IPA" && it.onlyOffer().format == BOTTLE }.noDesc()
+      ITEMS.first { it.name == "Work IPA" && it.offers.any { offer -> offer.format == CAN } }.noDesc()
     )
   }
 
@@ -50,9 +51,19 @@ class ForestRoadScraperTest {
 
   @Test
   fun `identifies mixed`() {
-    val item = ITEMS.byName("Mixed 12 Pack")
+    val item = ITEMS.byName("Forest Road Mixed")
 
     assertTrue(item.mixed)
     assertTrue(item.onlyOffer().quantity > 1)
+  }
+
+  @Test
+  fun `identifies sold-out`() {
+    assertFalse(ITEMS.byName("Tart Winter Ale Ting").available)
+  }
+
+  @Test
+  fun `ignores subscriptions`() {
+    assertFalse(ITEMS.any { it.name.containsWord("subscription") })
   }
 }
